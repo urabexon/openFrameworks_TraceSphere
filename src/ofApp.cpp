50daf24ac02b9eb2cad3dc8af51a1b96f8dd107d
urabexon <hiroki708.a48uk@gmail.com>
2025-06-01 23:43:51 +0900

    mouseReleased sunc setup

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -227,7 +227,7 @@ void ofApp::mousePressed(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    count++;
 }
 
 //--------------------------------------------------------------
